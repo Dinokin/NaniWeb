415d680dffe5d5f5c9818de7086fdbf646070d03
Micael Valentim <lord.valentim@gmail.com>
2019-01-04 19:23:42 -0300

    Finished user manager

--- NaniWeb/Controllers/UserManagerController.cs
+++ NaniWeb/Controllers/UserManagerController.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NaniWeb.Models.Users;
 
 namespace NaniWeb.Controllers
 {
@@ -10,26 +12,52 @@ namespace NaniWeb.Controllers
     {
         private readonly UserManager<IdentityUser<int>> _userManager;
 
+        public UserManagerController(UserManager<IdentityUser<int>> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> List()
-        {            
-            return View("UserList", await _userManager.Users.ToListAsync());
+        {
+            var model = new List<User>();
+            
+            foreach (var identityUser in _userManager.Users)
+            {
+                var roles = await _userManager.GetRolesAsync(identityUser);
+                
+                model.Add(new User
+                {
+                    Id = identityUser.Id,
+                    Username = identityUser.UserName,
+                    Role = roles.Count > 0 ? roles[0] : "None"
+                });
+            }
+            
+            return View("UserList", model);
         }
 
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> EditRole(int userId, string role)
         {
             var user = await _userManager.FindByIdAsync(userId.ToString());
-            await _userManager.RemoveFromRolesAsync(user, new[] {"Administrator", "Moderator", "Uploader"});
-            await _userManager.AddToRoleAsync(user, role);
+            await _userManager.RemoveFromRoleAsync(user, "Administrator");
+            await _userManager.RemoveFromRoleAsync(user, "Moderator");
+            await _userManager.RemoveFromRoleAsync(user, "Uploader");
+
+            if (role != "None")
+                await _userManager.AddToRoleAsync(user, role);
 
             return RedirectToAction("List");
         }
 
         [Authorize(Roles = "Administrator")]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> Delete(int userId)
         {
-            return null;
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            await _userManager.DeleteAsync(user);
+            
+            return RedirectToAction("List");
         }
     }
 }
